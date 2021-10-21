dbdc2d1768dde0508163f942bc4f1f8b72cb5cad
Murilo <murilotimo@gmail.com>
2021-10-21 00:01:15 -0300

    Ajustando a aversao da branch

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021051702;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2021051100;        // Requires this Moodle version.
+$plugin->requires  = 2018120309;        // Requires this Moodle version.
 $plugin->component = 'profilefield_autocomplete'; // Full name of the plugin (used for diagnostics)
